ae070f24ca750c97156c5036ab81730c25b7bd24
nsk0826 <nsk.20160401@gmail.com>
2022-03-31 00:11:36 +0900

    add test

--- src/useHumanType.test.ts
+++ src/useHumanType.test.ts
@@ -1,22 +1,32 @@
-import { makeHuman , humanName} from "./useHumanType";
-describe('useHuman' , (): void => {
-  test('makeHuman', (): void => {
-    expect(makeHuman(
-      "taro", 5)).toEqual({
-        name: "taro",
-        age: 5
-      });
+import { makeHuman, humanName, totalAge } from "./useHumanType";
+describe("useHuman", (): void => {
+  test("makeHuman", (): void => {
+    expect(makeHuman("taro", 5, "fukuoka")).toEqual({
+      name: "taro",
+      age: 5,
+      address: "fukuoka"
+    });
   });
-  test('humanName', (): void => {
-    const taro = makeHuman(
-      "taro", 5)
+  test("humanName", (): void => {
+    const taro = makeHuman("taro", 5, "tosu");
 
-    expect(humanName(taro)).toBe("taro")
+    expect(humanName(taro)).toBe("taro");
   });
-  test('humanName２', (): void => {
-    const taro = makeHuman(
-      "二郎", 5)
+  test("humanName２", (): void => {
+    const taro = makeHuman("二郎", 5, "saga");
 
-    expect(humanName(taro)).toBe("二郎")
-  });     
-})
+    expect(humanName(taro)).toBe("二郎");
+  });
+
+  test("totalAge", (): void => {
+    const ichiro = makeHuman("一郎", 40, "saga");
+    const jiro = makeHuman("二郎", 20, "saga");
+    const saburo = makeHuman("三郎", 10, "saga");
+
+    const humans = [ichiro,jiro,saburo];
+
+    expect(totalAge(humans)).toBe(70);
+  });
+
+
+});
