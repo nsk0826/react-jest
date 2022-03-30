c4bffaa112df43e27f3eaeac5b23910d481ea083
nsk0826 <nsk.20160401@gmail.com>
2022-03-31 00:07:24 +0900

    fix function

--- src/useHumanType.ts
+++ src/useHumanType.ts
@@ -1,19 +1,23 @@
 import { stringify } from "querystring";
-import {Human} from "./type/Human";
+import { createSecureContext } from "tls";
+import { Human } from "./type/Human";
 
-export function makeHuman(name : string , age: number, address: string): Human {
+export function makeHuman(name: string, age: number, address: string): Human {
   const newHuman = {
-    name : name,
+    name: name,
     age: age,
     address: address,
-  }
+  };
   return newHuman;
-};
+}
 
 export function humanName(human: Human): string {
   return human.name;
 }
 
-export function totalAge (...human : Human) : number {
-  
+export function totalAge(humans: Human[]): number {
+  const total:number = humans.reduce((p: number, current: Human): number => {
+    return p + current.age;
+  },0);
+  return total;
 }
