c1813141a4af8bf86daf7b9ebbb596ebf3533beb
nsk0826 <nsk.20160401@gmail.com>
2022-04-20 00:09:53 +0900

    add tesut

--- src/useHumanType.test.ts
+++ src/useHumanType.test.ts
@@ -5,8 +5,9 @@ import {
   numberHumanAddress,
   isNumberBiggerFor,
   isNumberMiniFor,
-  getNowDate,
+  makeProf
 } from "./useHumanType";
+
 describe("useHuman", (): void => {
   test("makeHuman", (): void => {
     expect(makeHuman("taro", 5, "fukuoka")).toEqual({
@@ -153,9 +154,9 @@ describe("useHuman", (): void => {
   });
 
   test.each([
-    [{ name: "一郎", age: 10, address: "tokyo" },"一郎"],
-    [{ name: "次郎", age: 8, address: "saga" }, "次郎"],
-  ])(".numberHumanAddress (%o, %p)", (a, expected) => {
-    expect(numberHumanAddress(,)).toBe(expected);
+    [{ name: "一郎", age: 10, address: "tokyo" },"一郎は10歳"],
+    [{ name: "次郎", age: 8, address: "saga" }, "次郎は8歳"],
+  ])(".numberHumanAddress (%p, %p)", (a, expected) => {
+    expect(makeProf(a)).toBe(expected);
   });
 });
